5fd4cd9c943c408adb08dd8da97e54087cf9c2f2
Yuliyakulikova <uskulikova@yandex.ru>
2024-01-10 16:21:21 +0500

    test

---
+++ school/views.py
@@ -0,0 +1,14 @@
+from django.views.generic import ListView
+from django.shortcuts import render
+
+from .models import Student
+
+
+def students_list(request):
+    template = 'school/students_list.html'
+    ordering = 'group'
+    r = Student.objects.all().prefetch_related('teachers').order_by(ordering)
+    context = {'object_list': r}
+    return render(request, template, context)
+    # используйте этот параметр для упорядочивания результатов
+    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
